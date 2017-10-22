aedd080d50f2e105c265dcc9375afc99e3572bbd
Srdjan Milic <SrdjanRMilic@gmail.com>
2017-10-22 14:49:20 +0200

    first local commit / setting up git

---
+++ js/myScript.js
@@ -0,0 +1,7 @@
+var testElement = document.createElement("p");
+var node = document.createTextNode("This is newly added <p></p> Element. JavaScript is Working!!");
+testElement.appendChild(node);
+
+var element = document.getElementById("results");
+element.appendChild(testElement);
+
