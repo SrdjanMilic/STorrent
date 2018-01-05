d330c9a31cff51c6b7af8139cf853aa207ac4667
Srdjan <SrdjanRMilic@gmail.com>
2018-01-05 08:58:27 +0100

    massive changes on functionality and UI

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
-# STorrent
-Search for torrents on multiple sites
+STorrent - Search for torrents on multiple sites
 
-This application is at an early stage of development and it's not for use!
+Electron multi platform desktop application.
+
+All default providers are:
+torrentSearch.enableProvider('ThePirateBay');	// public
+torrentSearch.disableProvider('Yggtorrent');	// authentication
+torrentSearch.disableProvider('KickassTorrents');	// public
+torrentSearch.enableProvider('TorrentProject');	// public
+torrentSearch.enableProvider('Rarbg');	// public
+torrentSearch.enableProvider('Torrent9');	// public
+torrentSearch.enableProvider('Torrentz2');	// public
+torrentSearch.disableProvider('IpTorrents');	// authentication
+torrentSearch.disableProvider('TorrentLeech');	// authentication
+torrentSearch.disableProvider('1337x');	// public
+torrentSearch.enableProvider('ExtraTorrent');	// public
+
+You can toggle the state of providers by renaming '...disableProvider...' to '...enableProvider...' in myScript.js file.
+
+This application is at an early stage of development and has a very basic functionality for now!
+
+New features and improvements will be implemented as time goes by.
 
 STorrent is based on the 'torrent-search-api' by Jimmy Laurent:
 https://github.com/JimmyLaurent/torrent-search-api
+
+For customization options please follow the link above.
--- js/myScript.js
+++ js/myScript.js
@@ -1,24 +1,125 @@
-var testElement = document.createElement("p");
-var node = document.createTextNode("This is newly added <p></p> Element. JavaScript is Working!!");
-testElement.appendChild(node);
-
+// const BrowserWindow = require('electron').remote.BrowserWindow;
 const TorrentSearchApi = require('torrent-search-api');
 const torrentSearch = new TorrentSearchApi();
+const $ = require('jquery');
 
-var element = document.getElementById('JavaScript-test');
-element.appendChild(testElement);
+// Search providers
 
-torrentSearch.enableProvider('KickassTorrents');
+torrentSearch.enableProvider('ThePirateBay');	// public
+torrentSearch.disableProvider('Yggtorrent');	// authentication
+torrentSearch.disableProvider('KickassTorrents');	// public
+torrentSearch.enableProvider('TorrentProject');	// public
+torrentSearch.enableProvider('Rarbg');	// public
+torrentSearch.enableProvider('Torrent9');	// public
+torrentSearch.enableProvider('Torrentz2');	// public
+torrentSearch.disableProvider('IpTorrents');	// authentication
+torrentSearch.disableProvider('TorrentLeech');	// authentication
+torrentSearch.disableProvider('1337x');	// public
+torrentSearch.enableProvider('ExtraTorrent');	// public
 
-torrentSearch.search('ufc', '', 2)
-  .then(torrents => {
-    // console.log(torrents);
-    // document.getElementById('results').innerHTML = JSON.stringify(torrents[0]['title'] + ', ' + torrents[0]['time'] + ', ' + torrents[1]['title'] + ', ' + torrents[1]['time']);
-    document.getElementById('results').innerHTML = JSON.stringify(torrents);
-  })
-  .catch(err => {
-    window.alert(err);
-  });
+function searchResults() {
+	torrentSearch.search(searchTerm.value, '', 5)
+		.then(torrents => {
 
-// var results = JSON.parse(torrents);
-// document.getElementById('results').innerHTML = torrents[0] + torrents[1] + torrents[2] + torrents[3];
+			// URL create from JSON output
+			let magnetString = JSON.stringify(torrents['0'].magnet).substr(1).slice(0, -1);
+			let magnetLink = '<a href="' + magnetString + '" class="btn btn-default btn-xs btn-block" role="button"' + '>Download</a>';
+
+			document.getElementById('title-1').innerHTML = JSON.stringify(torrents['0'].title).substr(1).slice(0, -1);
+			document.getElementById('title-2').innerHTML = JSON.stringify(torrents['1'].title).substr(1).slice(0, -1);
+			document.getElementById('title-3').innerHTML = JSON.stringify(torrents['2'].title).substr(1).slice(0, -1);
+			document.getElementById('title-4').innerHTML = JSON.stringify(torrents['3'].title).substr(1).slice(0, -1);
+			document.getElementById('title-5').innerHTML = JSON.stringify(torrents['4'].title).substr(1).slice(0, -1);
+
+			document.getElementById('time-1').innerHTML = JSON.stringify(torrents['0'].time).substr(1).slice(0, -1);
+			document.getElementById('time-2').innerHTML = JSON.stringify(torrents['1'].time).substr(1).slice(0, -1);
+			document.getElementById('time-3').innerHTML = JSON.stringify(torrents['2'].time).substr(1).slice(0, -1);
+			document.getElementById('time-4').innerHTML = JSON.stringify(torrents['3'].time).substr(1).slice(0, -1);
+			document.getElementById('time-5').innerHTML = JSON.stringify(torrents['4'].time).substr(1).slice(0, -1);
+
+			// document.getElementById('seeds-1').innerHTML = JSON.stringify(torrents['0'].seeds);
+			// document.getElementById('seeds-2').innerHTML = JSON.stringify(torrents['1'].seeds);
+			// document.getElementById('seeds-3').innerHTML = JSON.stringify(torrents['2'].seeds);
+			// document.getElementById('seeds-4').innerHTML = JSON.stringify(torrents['3'].seeds);
+			// document.getElementById('seeds-5').innerHTML = JSON.stringify(torrents['4'].seeds);
+
+			document.getElementById('peers-1').innerHTML = JSON.stringify(torrents['0'].peers);
+			document.getElementById('peers-2').innerHTML = JSON.stringify(torrents['1'].peers);
+			document.getElementById('peers-3').innerHTML = JSON.stringify(torrents['2'].peers);
+			document.getElementById('peers-4').innerHTML = JSON.stringify(torrents['3'].peers);
+			document.getElementById('peers-5').innerHTML = JSON.stringify(torrents['4'].peers);
+
+			document.getElementById('size-1').innerHTML = JSON.stringify(torrents['0'].size).substr(1).slice(0, -1);
+			document.getElementById('size-2').innerHTML = JSON.stringify(torrents['1'].size).substr(1).slice(0, -1);
+			document.getElementById('size-3').innerHTML = JSON.stringify(torrents['2'].size).substr(1).slice(0, -1);
+			document.getElementById('size-4').innerHTML = JSON.stringify(torrents['3'].size).substr(1).slice(0, -1);
+			document.getElementById('size-5').innerHTML = JSON.stringify(torrents['4'].size).substr(1).slice(0, -1);
+
+			// document.getElementById('url-1').innerHTML = ''<a href="' + urlString + '"' + class="magnet-url"' + '>click here please</a>'';
+			// document.getElementById('url-1').innerHTML = JSON.stringify(torrents['0'].magnet).substr(1).slice(0, -1);
+			document.getElementById('url-1').innerHTML = magnetLink;
+			document.getElementById('url-2').innerHTML = magnetLink;
+			document.getElementById('url-3').innerHTML = magnetLink;
+			document.getElementById('url-4').innerHTML = magnetLink;
+			document.getElementById('url-5').innerHTML = magnetLink;
+
+			document.getElementById('provider-1').innerHTML = JSON.stringify(torrents['0'].provider).substr(1).slice(0, -1);
+			document.getElementById('provider-2').innerHTML = JSON.stringify(torrents['1'].provider).substr(1).slice(0, -1);
+			document.getElementById('provider-3').innerHTML = JSON.stringify(torrents['2'].provider).substr(1).slice(0, -1);
+			document.getElementById('provider-4').innerHTML = JSON.stringify(torrents['3'].provider).substr(1).slice(0, -1);
+			document.getElementById('provider-5').innerHTML = JSON.stringify(torrents['4'].provider).substr(1).slice(0, -1);
+
+			// Call for torrent objects output used for troubleshooting purposes
+			// document.getElementById('results').innerHTML = JSON.stringify(torrents);
+		})
+
+		.catch(err => {
+			window.alert(err);
+		});
+}
+
+// Check for providers
+// document.getElementById('providers').innerHTML = JSON.stringify(torrentSearch.getProviders());
+// document.getElementById('active-providers').innerHTML = JSON.stringify(torrentSearch.getActiveProviders());
+
+// Spinner
+// Event listeners
+
+let searchTerm = document.getElementById('search-term');
+
+const webView = document.querySelector('webview');
+let hideWebView = document.getElementById('webview').style.display = "none";
+
+searchTerm.addEventListener('keypress', function () {
+	if (event.keyCode === 13) {
+
+		document.getElementById('webview').style.display = "flex";
+
+		searchResults();
+
+		// let waitForResults = webview.isWaitingForResponse(searchResults());
+		// if (waitForResults == true) {
+		// 	document.getElementById('webview').style.display = "none";
+		// 	webView.scrollTop();
+		// }
+
+
+		// document.getElementById('webview').style.display = "none";
+
+		webView.addEventListener('did-get-redirect-request', () => {
+			webView.openDevTools()
+		})
+
+	}
+
+});
+
+document.getElementById('btn-search').addEventListener('click', function () {
+	searchResults();
+});
+
+
+
+
+// webview.addEventListener('did-start-loading', loadstart)
+// webview.addEventListener('did-stop-loading', loadstop)
