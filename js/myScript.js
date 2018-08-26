5c11d771dd3a22495c6a45bd028dbf3c1050c832
Srdjan Milic <srdjanrmilic@gmail.com>
2018-08-26 16:52:26 +0200

    some code linting and fixes

--- js/myScript.js
+++ js/myScript.js
@@ -1,11 +1,13 @@
+/* eslint-env browser */
 const TorrentSearchApi = require('torrent-search-api');
+
 const torrentSearch = new TorrentSearchApi();
 const $ = require('jquery');
 require('datatables.net-bs')();
 
 // Search providers
 
-torrentSearch.enableProvider('ThePirateBay'); // public
+torrentSearch.disableProvider('ThePirateBay'); // public
 torrentSearch.disableProvider('Yggtorrent'); // authentication
 torrentSearch.disableProvider('KickassTorrents'); // public
 torrentSearch.disableProvider('TorrentProject'); // public
@@ -17,222 +19,217 @@ torrentSearch.disableProvider('TorrentLeech'); // authentication
 torrentSearch.disableProvider('1337x'); // public
 torrentSearch.disableProvider('ExtraTorrent'); // public
 
-let body = document.getElementById('body');
-let spinner = document.getElementById('spinner');
-let overlay = document.getElementById('overlay');
-let tableResults = document.getElementById('table-results');
-let searchTerm = document.getElementById('search-term');
+const body = $('#body');
+const tableResults = $('#table-results');
+const searchTerm = $('#search-term');
 
 function removeTable() {
-	$('#table-wrapper').remove();
+  $('#table-wrapper').remove();
 }
 
-document.getElementById('btn-refresh').addEventListener('click', function() {
-	location.reload();
+$('#btn-refresh').addEventListener('click', () => {
+  location.reload();
 });
 
 // JQuery detach spinner
-$(spinner).detach();
-$(overlay).detach();
-
-searchTerm.addEventListener('keypress', function() {
-	if (event.keyCode === 13) {
-		$(spinner).appendTo(body);
-		$(overlay).appendTo(body);
-		$('#DataTables_Table_0_wrapper').remove();
-		$('#DataTables_Table_1_wrapper').remove();
-		$('#DataTables_Table_2_wrapper').remove();
-		$('#DataTables_Table_4_wrapper').remove();
-		$('#DataTables_Table_5_wrapper').remove();
-		removeTable();
-		searchResults();
-	}
-});
-
-document.getElementById('btn-search').addEventListener('click', function() {
-	$(overlay).appendTo(body);
-	$(spinner).appendTo(body);
-	// Remove search results after consecutive search.
-	// It's not a final solution. Need to be fixed.
-	$('#DataTables_Table_0_wrapper').remove();
-	$('#DataTables_Table_1_wrapper').remove();
-	$('#DataTables_Table_2_wrapper').remove();
-	$('#DataTables_Table_4_wrapper').remove();
-	$('#DataTables_Table_5_wrapper').remove();
-	removeTable();
-	searchResults();
-});
+$('#spinner').detach();
+$('#overlay').detach();
 
 function searchResults() {
-	torrentSearch.search(searchTerm.value, '', '')
-		.then(torrents => {
+  torrentSearch.search(searchTerm.value, '', '')
+    .then((torrents) => {
+      // Dynamic table creation
+      const table = document.createElement('table');
+      table.className = 'table table-condensed table-bordered table-hover table-striped';
 
-			// Dynamic table creation
-			let table = document.createElement('table');
-			table.className = 'table table-condensed table-bordered table-hover table-striped';
+      const tableBody = document.createElement('tbody');
+      tableBody.id = 'table-body';
 
-			let tableBody = document.createElement('tbody');
-			tableBody.id = 'table-body';
+      const tableColumnHeader = document.createElement('thead');
+      const columnHeaderRow = document.createElement('tr');
+      table.appendChild(tableColumnHeader);
+      tableColumnHeader.appendChild(columnHeaderRow);
 
-			let tableColumnHeader = document.createElement('thead');
-			let columnHeaderRow = document.createElement('tr');
-			table.appendChild(tableColumnHeader);
-			tableColumnHeader.appendChild(columnHeaderRow);
+      // Creates table header elements
+      for (let th = 0; th < 8; th++) {
+        const columnHeaderCells = document.createElement('th');
+        columnHeaderRow.appendChild(columnHeaderCells);
+      }
 
-			// Creates table header elements
-			for (let th = 0; th < 8; th++) {
-				let columnHeaderCells = document.createElement('th');
-				columnHeaderRow.appendChild(columnHeaderCells);
-			}
+      // Creates Titles for column
+      const firstCell = table.getElementsByTagName('th')[0];
+      const columnHeaderTextCell1 = document.createTextNode('#');
+      firstCell.appendChild(columnHeaderTextCell1);
 
-			// Creates Titles for column
-			let firstCell = table.getElementsByTagName('th')[0];
-			let columnHeaderTextCell1 = document.createTextNode('#');
-			firstCell.appendChild(columnHeaderTextCell1);
+      const secondCell = table.getElementsByTagName('th')[1];
+      const columnHeaderTextCell2 = document.createTextNode('Title');
+      secondCell.appendChild(columnHeaderTextCell2);
 
-			let secondCell = table.getElementsByTagName('th')[1];
-			let columnHeaderTextCell2 = document.createTextNode('Title');
-			secondCell.appendChild(columnHeaderTextCell2);
+      const thirdCell = table.getElementsByTagName('th')[2];
+      const columnHeaderTextCell3 = document.createTextNode('Time');
+      thirdCell.appendChild(columnHeaderTextCell3);
 
-			let thirdCell = table.getElementsByTagName('th')[2];
-			let columnHeaderTextCell3 = document.createTextNode('Time');
-			thirdCell.appendChild(columnHeaderTextCell3);
+      const fourthCell = table.getElementsByTagName('th')[3];
+      const columnHeaderTextCell4 = document.createTextNode('Seeds');
+      fourthCell.appendChild(columnHeaderTextCell4);
 
-			let fourthCell = table.getElementsByTagName('th')[3];
-			let columnHeaderTextCell4 = document.createTextNode('Seeds');
-			fourthCell.appendChild(columnHeaderTextCell4);
+      const fifthCell = table.getElementsByTagName('th')[4];
+      const columnHeaderTextCell5 = document.createTextNode('Peers');
+      fifthCell.appendChild(columnHeaderTextCell5);
 
-			let fifthCell = table.getElementsByTagName('th')[4];
-			let columnHeaderTextCell5 = document.createTextNode('Peers');
-			fifthCell.appendChild(columnHeaderTextCell5);
+      const sixthCell = table.getElementsByTagName('th')[5];
+      const columnHeaderTextCell6 = document.createTextNode('Size');
+      sixthCell.appendChild(columnHeaderTextCell6);
 
-			let sixthCell = table.getElementsByTagName('th')[5];
-			let columnHeaderTextCell6 = document.createTextNode('Size');
-			sixthCell.appendChild(columnHeaderTextCell6);
+      const seventhCell = table.getElementsByTagName('th')[6];
+      const columnHeaderTextCell7 = document.createTextNode('URL');
+      seventhCell.appendChild(columnHeaderTextCell7);
 
-			let seventhCell = table.getElementsByTagName('th')[6];
-			let columnHeaderTextCell7 = document.createTextNode('URL');
-			seventhCell.appendChild(columnHeaderTextCell7);
+      const eightCell = table.getElementsByTagName('th')[7];
+      const columnHeaderTextCell8 = document.createTextNode('Provider');
+      eightCell.appendChild(columnHeaderTextCell8);
 
-			let eightCell = table.getElementsByTagName('th')[7];
-			let columnHeaderTextCell8 = document.createTextNode('Provider');
-			eightCell.appendChild(columnHeaderTextCell8);
+      // Creates each table row
+      let titleString = '';
+      let timeString = '';
+      let seedsString = '';
+      for (let row = 0; row < torrents.length; row++) {
+        const rowHeader = document.createElement('th');
+        const rowId = document.createTextNode(row + 1);
+        rowHeader.appendChild(rowId);
+        const tableRow = document.createElement('tr');
+        tableRow.appendChild(rowHeader);
 
-			// Creates each table row
-			let titleString = '';
-			let timeString = '';
-			let seedsString = '';
-			for (let row = 0; row < torrents.length; row++) {
-				let rowHeader = document.createElement('th');
-				let rowId = document.createTextNode(row + 1);
-				rowHeader.appendChild(rowId);
-				let tableRow = document.createElement('tr');
-				tableRow.appendChild(rowHeader);
+        // Title String
+        const tableRowData = document.createElement('td');
+        tableRow.appendChild(tableRowData);
 
-				// Title String
-				let tableRowData = document.createElement('td');
-				tableRow.appendChild(tableRowData);
+        titleString = JSON.stringify(torrents[row].title).substr(1).slice(0, -1);
+        const titleText = document.createTextNode(titleString);
+        tableRowData.appendChild(titleText);
 
-				titleString = JSON.stringify(torrents[row].title).substr(1).slice(0, -1);
-				let titleText = document.createTextNode(titleString);
-				tableRowData.appendChild(titleText);
+        // Time String
+        const timeRowData = document.createElement('td');
+        tableRow.appendChild(timeRowData);
 
-				// Time String
-				let timeRowData = document.createElement('td');
-				tableRow.appendChild(timeRowData);
+        timeString = JSON.stringify(torrents[row].time).substr(1).slice(0, -1);
+        const timeText = document.createTextNode(timeString);
+        timeRowData.appendChild(timeText);
 
-				timeString = JSON.stringify(torrents[row].time).substr(1).slice(0, -1);
-				let timeText = document.createTextNode(timeString);
-				timeRowData.appendChild(timeText);
+        // Seeds String
+        const seedsRowData = document.createElement('td');
+        tableRow.appendChild(seedsRowData);
 
-				// Seeds String
-				let seedsRowData = document.createElement('td');
-				tableRow.appendChild(seedsRowData);
+        seedsString = JSON.stringify(torrents[row].seeds);
+        const seedsText = document.createTextNode(seedsString);
+        seedsRowData.appendChild(seedsText);
 
-				seedsString = JSON.stringify(torrents[row].seeds);
-				let seedsText = document.createTextNode(seedsString);
-				seedsRowData.appendChild(seedsText);
+        // Peers String
+        const peersRowData = document.createElement('td');
+        tableRow.appendChild(peersRowData);
 
-				// Peers String
-				let peersRowData = document.createElement('td');
-				tableRow.appendChild(peersRowData);
+        const peersString = JSON.stringify(torrents[row].peers);
+        const peersText = document.createTextNode(peersString);
+        peersRowData.appendChild(peersText);
 
-				let peersString = JSON.stringify(torrents[row].peers);
-				let peersText = document.createTextNode(peersString);
-				peersRowData.appendChild(peersText);
+        // Size String
+        const sizeRowData = document.createElement('td');
+        tableRow.appendChild(sizeRowData);
 
-				// Size String
-				let sizeRowData = document.createElement('td');
-				tableRow.appendChild(sizeRowData);
+        const sizeString = JSON.stringify(torrents[row].size).substr(1).slice(0, -1);
+        const sizeText = document.createTextNode(sizeString);
+        sizeRowData.appendChild(sizeText);
 
-				let sizeString = JSON.stringify(torrents[row].size).substr(1).slice(0, -1);
-				let sizeText = document.createTextNode(sizeString);
-				sizeRowData.appendChild(sizeText);
+        // URL String
+        const urlRowData = document.createElement('td');
+        tableRow.appendChild(urlRowData);
 
-				// URL String
-				let urlRowData = document.createElement('td');
-				tableRow.appendChild(urlRowData);
+        const magnetString = JSON.stringify(torrents[row].magnet).substr(1).slice(0, -1);
+        const createLink = document.createElement('a');
+        createLink.href = magnetString;
+        createLink.setAttribute('class', 'btn btn-default btn-xs btn-block');
+        createLink.setAttribute('role', 'button');
+        createLink.innerHTML = 'Download';
 
-				let magnetString = JSON.stringify(torrents[row].magnet).substr(1).slice(0, -1);
-				let createLink = document.createElement('a');
-				createLink.href = magnetString;
-				createLink.setAttribute('class', 'btn btn-default btn-xs btn-block');
-				createLink.setAttribute('role', 'button');
-				createLink.innerHTML = 'Download';
+        urlRowData.appendChild(createLink);
 
-				urlRowData.appendChild(createLink);
+        // Provider String
+        const providerRowData = document.createElement('td');
+        tableRow.appendChild(providerRowData);
 
-				// Provider String
-				let providerRowData = document.createElement('td');
-				tableRow.appendChild(providerRowData);
+        const providerString = JSON.stringify(torrents[row].provider).substr(1).slice(0, -1);
+        const providerText = document.createTextNode(providerString);
+        providerRowData.appendChild(providerText);
 
-				let providerString = JSON.stringify(torrents[row].provider).substr(1).slice(0, -1);
-				let providerText = document.createTextNode(providerString);
-				providerRowData.appendChild(providerText);
+        // add the row to the end of the table body
+        tableBody.appendChild(tableRow);
+      }
 
-				// add the row to the end of the table body
-				tableBody.appendChild(tableRow);
-			}
+      tableResults.appendChild(table);
+      table.appendChild(tableBody);
+    })
 
-			tableResults.appendChild(table);
-			table.appendChild(tableBody);
+    .then(() => {
+      $('table').DataTable({
+        autoWidth: false,
+        lengthChange: true,
+        pageLength: 15,
+        lengthMenu: [15, 25, 50, 100],
+      });
 
-		})
+      $('#overlay').detach();
+      $('#spinner').detach();
+    })
 
-		.then(function() {
-			$('table').DataTable({
-				'autoWidth': false,
-				'lengthChange': true,
-				'pageLength': 15,
-				'lengthMenu': [15, 25, 50, 100]
-			});
-
-			$(overlay).detach();
-			$(spinner).detach();
-		})
-
-		.catch(err => {
-			window.alert('Error occurred!' + '\r\n' + err + '\r\n' + 'Please, try again.');
-			$(overlay).detach();
-			$(spinner).detach();
-			$(body).remove();
-			location.reload();
-		});
-
+    .catch((err) => {
+      window.alert(`${'Error occurred!' + '\r\n'}${err}\r\n` + 'Please, try again.');
+      $('#overlay').detach();
+      $('#spinner').detach();
+      $(body).remove();
+      location.reload();
+    });
 }
+
+searchTerm.addEventListener('keypress', () => {
+  if (event.keyCode === 13) {
+    $('#spinner').appendTo(body);
+    $('overlay').appendTo(body);
+    $('#DataTables_Table_0_wrapper').remove();
+    $('#DataTables_Table_1_wrapper').remove();
+    $('#DataTables_Table_2_wrapper').remove();
+    $('#DataTables_Table_4_wrapper').remove();
+    $('#DataTables_Table_5_wrapper').remove();
+    removeTable();
+    searchResults();
+  }
+});
+
+document.getElementById('btn-search').addEventListener('click', () => {
+  $('#overlay').appendTo(body);
+  $('#spinner').appendTo(body);
+  // Remove search results after consecutive search.
+  // It's not a final solution. Need to be fixed.
+  $('#DataTables_Table_0_wrapper').remove();
+  $('#DataTables_Table_1_wrapper').remove();
+  $('#DataTables_Table_2_wrapper').remove();
+  $('#DataTables_Table_4_wrapper').remove();
+  $('#DataTables_Table_5_wrapper').remove();
+  removeTable();
+  searchResults();
+});
 
 // DataTables settings
 $.extend($.fn.dataTable.defaults, {
-	searching: true,
-	ordering: true,
-	retrieve: true
+  searching: true,
+  ordering: true,
+  retrieve: true,
 });
 
 $('table').DataTable({
-	'autoWidth': false,
-	'lengthChange': true,
-	'pageLength': 15,
-	'lengthMenu': [15, 25, 50, 100],
+  autoWidth: false,
+  lengthChange: true,
+  pageLength: 15,
+  lengthMenu: [15, 25, 50, 100],
 });
 
 // Check for providers
--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-const { app, BrowserWindow } = require('electron');
+const {
+  app,
+  BrowserWindow,
+} = require('electron');
 const path = require('path');
 const url = require('url');
 require('electron-context-menu')({
-	prepend: (params) => [{
-		label: 'Rainbow',
-		// Only show it when right-clicking images
-		visible: params.mediaType === 'image'
-	}]
+  prepend: params => [{
+    label: 'Rainbow',
+    // Only show it when right-clicking images
+    visible: params.mediaType === 'image',
+  }],
 });
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -14,30 +17,35 @@ require('electron-context-menu')({
 let win;
 
 function createWindow() {
-	// Create the browser window.
-	win = new BrowserWindow({ width: 1120, height: 700, show: false, icon: 'images/storrent.png'});
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 1100,
+    height: 800,
+    show: false,
+    icon: 'images/storrent.png',
+  });
 
-	// and load the index.html of the app.
-	win.loadURL(url.format({
-		pathname: path.join(__dirname, 'index.html'),
-		protocol: 'file:',
-		slashes: true
-	}));
+  // and load the index.html of the app.
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true,
+  }));
 
-	// Open the DevTools.
-	// win.webContents.openDevTools()
+  // Open the DevTools.
+  // win.webContents.openDevTools()
 
-	// Emitted when the window is closed.
-	win.on('closed', () => {
-		// Dereference the window object, usually you would store windows
-		// in an array if your app supports multi windows, this is the time
-		// when you should delete the corresponding element.
-		win = null;
-	});
-	
-	win.once('ready-to-show', () => {
-		win.show();
-	});
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null;
+  });
+
+  win.once('ready-to-show', () => {
+    win.show();
+  });
 }
 
 // This method will be called when Electron has finished
@@ -48,13 +56,12 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-	// On macOS it is common for applications and their menu bar
-	// to stay active until the user quits explicitly with Cmd + Q
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-
