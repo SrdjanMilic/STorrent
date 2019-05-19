b0847198d327ea305246100ca659fa7b14b1731f
Srdjan Milic <srdjanrmilic@gmail.com>
2019-05-19 19:25:24 +0200

    UI/UX, code and other fixes

--- README.md
+++ README.md
@@ -1,33 +1,30 @@
 # STorrent - Search for torrents with multiple providers
 
-**Electron multi platform desktop application**
+## Electron multi platform desktop application
 
 Torrent search providers are:
 
-torrentSearch.disableProvider("TorrentLeech") // authentication <br>
-torrentSearch.disableProvider("IpTorrents") // authentication <br>
-torrentSearch.disableProvider("Torrent9") // public <br>
-torrentSearch.enableProvider("Torrentz2") // public <br>
-torrentSearch.disableProvider("1337x") // public (fast response) <br>
-torrentSearch.disableProvider("ThePirateBay") // public <br>
-torrentSearch.disableProvider("Yggtorrent") // authentication <br>
-torrentSearch.disableProvider("KickassTorrents") // public <br>
-torrentSearch.enableProvider("Rarbg") // public (fast response) <br>
-torrentSearch.disableProvider("TorrentProject") // public <br>
-torrentSearch.enableProvider("ExtraTorrent") // public (fast response)
+torrentSearch.disableProvider("TorrentLeech") // authentication  
+torrentSearch.disableProvider("IpTorrents") // authentication  
+torrentSearch.disableProvider("Torrent9") // public  
+torrentSearch.enableProvider("Torrentz2") // public  
+torrentSearch.disableProvider("1337x") // public (fast response)  
+torrentSearch.disableProvider("ThePirateBay") // public  
+torrentSearch.disableProvider("Yggtorrent") // authentication  
+torrentSearch.enableProvider("KickassTorrents") // public  
+torrentSearch.enableProvider("Rarbg") // public (fast response)  
+torrentSearch.disableProvider("TorrentProject") // public  
+torrentSearch.disableProvider("ExtraTorrent") // public (fast response)
 
-You can toggle the active state of providers by renaming "...disableProvider..." to "...enableProvider..." in "js/index.js" file. Please note, if there is many providers enabled, waiting for search results could prolongs a bit.
+You can toggle the active state of providers by renaming "...disableProvider..." to "...enableProvider..." in "js/index.js" file. Please note, if there is many providers enabled, waiting for search results could prolongs a bit. Also, some providers are not working as it should causing the app to fail with response.
 
 To locally start the application from source, you will need to have [node.js](https://nodejs.org/en/) installed, then just cd to directory and run:
 
 1. npm update
 2. npm start
 
-<br>
-
 In section [release](https://github.com/SrdjanMilic/STorrent/releases), there is distributable builds for Linux and Windows. If you want to run app from distributable build, just double click on "storrent" executable in root directory.
 
-<br>
+STorrent is based on 'torrent-search-api' by [Jimmy Laurent](https://github.com/JimmyLaurent/torrent-search-api).
 
-STorrent is based on 'torrent-search-api' by [Jimmy Laurent](https://github.com/JimmyLaurent/torrent-search-api). <br>
 You can follow up above link for more options and configurations.
--- js/index.js
+++ js/index.js
@@ -1,160 +1,169 @@
-require("datatables.net-bs4")()
+require('datatables.net-bs4')();
 
-const $ = require("jquery")
-const jQuery = require("jquery")
-const fakeLoader = require("jquery.fakeloader")
-const TorrentSearchApi = require("torrent-search-api")
-const torrentSearch = new TorrentSearchApi()
+const $ = require('jquery');
+const jQuery = require('jquery');
+const fakeLoader = require('jquery.fakeloader');
+const TorrentSearchApi = require('torrent-search-api');
+const torrentSearch = new TorrentSearchApi();
 
-torrentSearch.disableProvider("TorrentLeech") // authentication
-torrentSearch.disableProvider("IpTorrents") // authentication
-torrentSearch.disableProvider("Torrent9") // public
-torrentSearch.enableProvider("Torrentz2") // public (slow response)
-torrentSearch.disableProvider("1337x") // public (fast response)
-torrentSearch.disableProvider("ThePirateBay") // public
-torrentSearch.disableProvider("Yggtorrent") // authentication
-torrentSearch.disableProvider("KickassTorrents") // public
-torrentSearch.enableProvider("Rarbg") // public (fast response)
-torrentSearch.disableProvider("TorrentProject") // public
-torrentSearch.enableProvider("ExtraTorrent") // public
+torrentSearch.disableProvider('TorrentLeech'); // authentication
+torrentSearch.disableProvider('IpTorrents'); // authentication
+torrentSearch.disableProvider('Torrent9'); // public
+torrentSearch.enableProvider('Torrentz2'); // public (slow response)
+torrentSearch.disableProvider('1337x'); // public (fast response)
+torrentSearch.disableProvider('ThePirateBay'); // public
+torrentSearch.disableProvider('Yggtorrent'); // authentication
+torrentSearch.enableProvider('KickassTorrents'); // public
+torrentSearch.enableProvider('Rarbg'); // public (fast response)
+torrentSearch.disableProvider('TorrentProject'); // public
+torrentSearch.disableProvider('ExtraTorrent'); // public
 
-let searchTerm = document.getElementById("search-term")
-/* jshint ignore:start */
+let searchTerm = document.getElementById('search-term');
+
+searchTerm.focus(); // place cursor in the search input filed
+
 const findTorrents = async () => {
-  let torrents = await torrentSearch.search(searchTerm.value, "", "")
-  /* jshint ignore:end */
+  let torrents = await torrentSearch.search(searchTerm.value,'','');
 
   // Create table
-  const table = document.createElement("table")
-  table.className = "table table-sm table-condensed table-bordered table-hover table-striped"
+  const table = document.createElement('table');
+  table.className = 'table table-sm table-condensed table-bordered table-hover table-striped';
 
   // Create table head
-  let thead = table.createTHead()
+  let thead = table.createTHead();
 
   // Create table head row
-  let theadRow = thead.insertRow()
+  let theadRow = thead.insertRow();
 
   // Create eight table head row cells
   for (let i = 0; i < 8; i++) {
-    let th = document.createElement("th")
-    theadRow.appendChild(th)
+    let th = document.createElement('th');
+    theadRow.appendChild(th);
   }
 
   // Create eight table head row cells names
-  let tableHeadCellName = table.getElementsByTagName("th")
+  let tableHeadCellName = table.getElementsByTagName('th');
 
-  tableHeadCellName[0].innerText = "ID"
-  tableHeadCellName[1].innerText = "Title"
-  tableHeadCellName[2].innerText = "Date"
-  tableHeadCellName[3].innerText = "Seeds"
-  tableHeadCellName[4].innerText = "Peers"
-  tableHeadCellName[5].innerText = "Size"
-  tableHeadCellName[6].innerText = "Url"
-  tableHeadCellName[7].innerText = "Provider"
+  tableHeadCellName[0].innerText = 'ID';
+  tableHeadCellName[1].innerText = 'Title';
+  tableHeadCellName[2].innerText = 'Date';
+  tableHeadCellName[3].innerText = 'Seeds';
+  tableHeadCellName[4].innerText = 'Peers';
+  tableHeadCellName[5].innerText = 'Size';
+  tableHeadCellName[6].innerText = 'Url';
+  tableHeadCellName[7].innerText = 'Provider';
 
-  const tbody = table.appendChild(document.createElement("tbody"))
+  const tbody = table.appendChild(document.createElement('tbody'));
 
   // Loop for all torrent search results
   for (let i = 0; i < torrents.length; i++) {
-    // document.getElementById("loop-test").innerText = `Loop working ${i} times!!` // For debugging
 
     // Create table row
-    let newRow = tbody.insertRow()
+    let newRow = tbody.insertRow();
 
     // Create ID numbers
-    let id = newRow.insertCell()
-    id.appendChild(document.createTextNode(i + 1))
-    newRow.appendChild(id)
+    let id = newRow.insertCell();
+    id.appendChild(document.createTextNode(i + 1));
+    newRow.appendChild(id);
 
     // Create Title String
-    let title = newRow.insertCell()
-    title.appendChild(document.createTextNode(JSON.stringify(torrents[i].title).substr(1).slice(0, -1)))
-    newRow.appendChild(title)
-    
+    let title = newRow.insertCell();
+    title.appendChild(document.createTextNode(JSON.stringify(torrents[i].title).substr(1).slice(0, -1)));
+    newRow.appendChild(title);
+
+    // if (document.createTextNode(JSON.stringify(torrents[i].title).substr(1).slice(0, -1)) === '') {
+    //   alert('There is no torrents with that name!');
+    //   return;
+    // }
+
     // Create Time String
-    let time = newRow.insertCell()
-    time.appendChild(document.createTextNode(JSON.stringify(torrents[i].time).substr(1).slice(0, -1)))
-    newRow.appendChild(time)
+    let time = newRow.insertCell();
+    time.appendChild(document.createTextNode(JSON.stringify(torrents[i].time).substr(1).slice(0, -1)));
+    newRow.appendChild(time);
 
     // Create Seeds String
-    let seeds = newRow.insertCell()
-    seeds.appendChild(document.createTextNode(JSON.stringify(torrents[i].seeds)))
-    newRow.appendChild(seeds)
+    let seeds = newRow.insertCell();
+    seeds.appendChild(document.createTextNode(JSON.stringify(torrents[i].seeds)));
+    newRow.appendChild(seeds);
 
     // Create Peers String
-    let peers = newRow.insertCell()
-    peers.appendChild(document.createTextNode(JSON.stringify(torrents[i].peers)))
-    newRow.appendChild(peers)
+    let peers = newRow.insertCell();
+    peers.appendChild(document.createTextNode(JSON.stringify(torrents[i].peers)));
+    newRow.appendChild(peers);
 
     // Create Size String
-    let size = newRow.insertCell()
-    size.appendChild(document.createTextNode(JSON.stringify(torrents[i].size).substr(1).slice(0, -1)))
-    newRow.appendChild(size)
+    let size = newRow.insertCell();
+    size.appendChild(document.createTextNode(JSON.stringify(torrents[i].size).substr(1).slice(0, -1)));
+    newRow.appendChild(size);
 
     // URL String
-    const url = newRow.insertCell()
-    newRow.appendChild(url)
+    const url = newRow.insertCell();
+    newRow.appendChild(url);
 
-    const createLink = document.createElement("a")
-    createLink.href = JSON.stringify(torrents[i].magnet).substr(1).slice(0, -1)
-    createLink.setAttribute("class", "btn btn-secondary btn-sm")
-    createLink.innerText = "Download"
+    const createLink = document.createElement('a');
+    createLink.href = JSON.stringify(torrents[i].magnet).substr(1).slice(0, -1);
+    createLink.setAttribute('class', 'btn btn-secondary btn-sm');
+    createLink.innerText = 'Download';
 
-    url.appendChild(createLink)
+    url.appendChild(createLink);
 
     // Provider String
-    let provider = newRow.insertCell()
-    newRow.appendChild(provider)
-    provider.appendChild(document.createTextNode(JSON.stringify(torrents[i].provider).substr(1).slice(0, -1)))
-    document.getElementById("torrent-results").appendChild(table)
+    let provider = newRow.insertCell();
+    newRow.appendChild(provider);
+    provider.appendChild(document.createTextNode(JSON.stringify(torrents[i].provider).substr(1).slice(0, -1)));
+    document.getElementById('torrent-results').appendChild(table);
   }
 
   try {
-    torrents // Block of code to try
+    torrents;
   } catch (err) {
-    alert(`Error occurred!\r\n${err}.\r\nPlease, try again.`) // catchCode - Block of code to handle errors
-    return
+    alert(`Error occurred!\r\n${err}.\r\nPlease, try again.`);
+    return;
   }
-  // finally {
-  //   window.alert("Check for errors has finally finished!") // finallyCode - Block of code to be executed regardless of the try / catch result
-  // }
-  // document.getElementById("search-results").innerHTML = JSON.stringify(torrents) // for debugging purposes
 
-  $("table").DataTable({ // call data table api
+  $('table').DataTable({ // call data table api
     autoWidth: false,
     lengthChange: true,
     pageLength: 15,
     lengthMenu: [15, 50, 100],
     retrieve: true
-  })
-}
+  });
+
+};
 
 let loader = () => {
-  $("#fakeLoader").fakeLoader({
+  $('#fakeLoader').fakeLoader({
     timeToHide: 300,
-    spinner: "spinner2",
-    bgColor: "#006ADB"
-  })
-}
+    spinner: 'spinner2',
+    bgColor: '#006ADB'
+  });
+};
 
 let newSearch = () => {
-  loader()
-  $(".dataTables_wrapper").remove() // remove previous results table
-  findTorrents()
-}
+  loader();
+  $('.dataTables_wrapper').remove(); // remove previous results table
+  findTorrents();
+};
 
 // Event Listeners
-searchTerm.addEventListener("keypress", () => {
-  if (event.keyCode === 13) {
-    $("#fakeLoader").removeAttr("style") // this is needed for loader function to fire each time
-    $(".form").removeAttr("id") // remove class styles
-    newSearch()
+searchTerm.addEventListener('keypress', () => {
+  if (event.keyCode === 13 && searchTerm.value === '') {
+    alert('Please, input search term.');
+  } else if (event.keyCode === 13 && searchTerm.value !== '') {
+    $('#fakeLoader').removeAttr('style'); // this is needed for loader function to fire each time
+    $('.form').removeAttr('id'); // remove class styles
+    newSearch();
   }
-})
+});
 
-document.getElementById("btn-search").addEventListener("click", () => {
-  $("#fakeLoader").removeAttr("style")
-  $(".form").removeAttr("id") // remove class styles
-  newSearch()
-})
+document.getElementById('btn-search').addEventListener('click', () => {
+  if (searchTerm.value === '') {
+    alert('Please, input search term.');
+    searchTerm.focus();
+  } else if (searchTerm.value !== '') {
+    $('#fakeLoader').removeAttr('style');
+    $('.form').removeAttr('id'); // remove class styles
+    newSearch();
+  }
+});
 
